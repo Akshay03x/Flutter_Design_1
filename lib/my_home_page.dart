4881576ffb3cc40c0afe888168fd23132a0f7961
akshay <21010101029@darshan.ac.in>
2023-01-08 17:41:40 +0530

    .

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -13,8 +13,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     List l1 = [
-      Icons.ac_unit_rounded,
-      Icons.add_circle,
+      Icons.home_filled,
+      Icons.icecream_outlined,
       Icons.access_alarms,
       Icons.ac_unit_sharp,
       Icons.access_time_filled_outlined,
@@ -27,14 +27,18 @@ class _MyHomePageState extends State<MyHomePage> {
     String dropDownValue = 'Hello';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: SafeArea(
         child: Scaffold(
           bottomNavigationBar: CurvedNavigationBar(
-            backgroundColor: Colors.pink,
+            // buttonBackgroundColor: Colors.,
+            color: Colors.black,
+            backgroundColor: Colors.white,
             items: <Widget>[
-              Icon(Icons.add, size: 30),
-              Icon(Icons.list, size: 30),
-              Icon(Icons.compare_arrows, size: 30),
+              Icon(Icons.cake_outlined, size: 30, color: Colors.white),
+              Icon(Icons.add, size: 30, color: Colors.white),
+              Icon(Icons.list, size: 30, color: Colors.white),
+              Icon(Icons.compare_arrows, size: 30, color: Colors.white),
             ],
             onTap: (index) {
               //Handle button tap
@@ -43,12 +47,14 @@ class _MyHomePageState extends State<MyHomePage> {
           body: Column(
             children: [
               Expanded(
-                flex: 3,
+                flex: 2,
                 child: Row(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     Expanded(
                       flex: 4,
                       child: Container(
+                        // color: Colors.,
                         child: Text(
                           textAlign: TextAlign.center,
                           "Chose Cokkies\nFor Your Tea Party",
@@ -96,14 +102,12 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Expanded(
                 flex: 1,
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      for (var i in l1) CustomForExpand2(icon: i),
-                    ],
-                  ),
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    for (var i in l1) Center(child: CustomForExpand2(icon: i)),
+                  ],
                 ),
               ),
               Expanded(
@@ -113,50 +117,53 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Row(
                     children: [
                       Expanded(
-                        child: Container(
-                          child: TextButton(
-                            child: Icon(Icons.account_balance_rounded),
-                            onPressed: () {},
-                          ),
-                        ),
+                        child: CustomForExpand3(
+                            icon: Icons.monitor_outlined,
+                            alignment: Alignment.centerLeft),
                         flex: 2,
                       ),
                       Expanded(
                         child: Container(
-                            child: DropdownButton(
-                          value: item[0],
-                          items: item.map((item) {
-                            return DropdownMenuItem(
-                              child: Text(
-
-                                item,
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              value: item,
-                            );
-                          }).toList(),
-                          onChanged: (country) {
-                            print("You selected: $country");
-                          },
-                        )),
+                          child: DropdownButton(
+                            value: item[0],
+                            items: item.map(
+                              (item) {
+                                return DropdownMenuItem(
+                                  child: Text(
+                                    item,
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  value: item,
+                                );
+                              },
+                            ).toList(),
+                            onChanged: (country) {
+                              print("You selected: $country");
+                            },
+                          ),
+                        ),
                         flex: 3,
                       ),
                       Expanded(
-                        child: Container(
-                          child: TextButton(
-                            child: Icon(Icons.account_balance_rounded),
-                            onPressed: () {},
-                          ),
-                        ),
+                        child: CustomForExpand3(
+                            icon: Icons.segment,
+                            alignment: Alignment.centerRight),
                         flex: 2,
-                      )
+                      ),
                     ],
                   ),
                 ),
               ),
               Expanded(
-                flex: 5,
-                child: Container(color: Colors.pink),
+                flex: 6,
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    for (var i=0;i<=6;i++)
+                    CustomForExpand4(),
+                  ],
+                ),
               ),
             ],
           ),
@@ -166,13 +173,94 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget CustomForExpand2({required icon}) {
-    return FloatingActionButton(
-      backgroundColor: Colors.amberAccent,
-      onPressed: () {},
-      child: Icon(
-        icon,
-        size: 35,
-        color: Colors.black,
+    return Container(
+      margin: EdgeInsets.fromLTRB(5, 0, 5, 10),
+      child: FloatingActionButton(
+        backgroundColor: Colors.white70,
+        onPressed: () {},
+        child: Icon(
+          icon,
+          size: 35,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+
+  Widget CustomForExpand3({required icon, required alignment}) {
+    return Container(
+      alignment: alignment,
+      child: TextButton(
+        child: Icon(icon, color: Colors.black),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget CustomForExpand4() {
+    return  Container(
+      height: 400,
+      width: 340,
+      // alignment: Alignment.center,
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(40)),
+        color: Colors.pinkAccent,
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'cakephotos/cake2.jpg',
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(20),
+                  height: 70,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Hello",
+                      ),
+                      Spacer(),
+                      Container(
+                        margin: EdgeInsets.only(right: 5),
+                        // padding: EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStatePropertyAll(Colors.black),
+                              padding: MaterialStatePropertyAll(
+                                  EdgeInsets.fromLTRB(35, 25, 35, 25)),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.horizontal(
+                                      left: Radius.circular(40),
+                                      right: Radius.circular(40),
+                                    ),
+                                  ))),
+                          child: Icon(
+                            Icons.add_shopping_cart,
+                          ),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                flex: 3,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
