1baf6f3244761a6a17d4ad9670dc394125542bf4
akshay <21010101029@darshan.ac.in>
2023-01-11 22:19:22 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project2/my_home_page.dart';
 
 void main() {
-  runApp(MyHomePage());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    ),
+  );
 }
-
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,10 +1,11 @@
+import 'package:blurrycontainer/blurrycontainer.dart';
+import 'package:cool_dropdown/cool_dropdown.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project2/login_page/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyHomePage extends StatefulWidget {
-  // const MyHomePage({Key? key}) : super(key: key);
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -23,56 +24,86 @@ class _MyHomePageState extends State<MyHomePage> {
       Icons.account_box_rounded,
       Icons.accessible_rounded,
     ];
-    List item = ['item1', 'item2', 'item3', 'item4', 'item5'];
-    String dropDownValue = 'Hello';
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: SafeArea(
-        child: Scaffold(
-          bottomNavigationBar: CurvedNavigationBar(
-            // buttonBackgroundColor: Colors.,
-            color: Colors.black,
-            backgroundColor: Colors.white,
-            items: <Widget>[
-              Icon(Icons.cake_outlined, size: 30, color: Colors.white),
-              Icon(Icons.add, size: 30, color: Colors.white),
-              Icon(Icons.list, size: 30, color: Colors.white),
-              Icon(Icons.compare_arrows, size: 30, color: Colors.white),
-            ],
-            onTap: (index) {
-              //Handle button tap
-            },
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      flex: 4,
-                      child: Container(
-                        // color: Colors.,
-                        child: Text(
-                          textAlign: TextAlign.center,
-                          "Chose Cokkies\nFor Your Tea Party",
-                          style: GoogleFonts.getFont(
-                            'Nunito',
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
+    List dropdownItemList = [
+      {'label': 'apple', 'value': 'apple'},
+      {'label': 'banana', 'value': 'banana'},
+      {'label': 'grape', 'value': 'grape'},
+      {'label': 'pineapple', 'value': 'pineapple'},
+      {'label': 'grape fruit', 'value': 'grape fruit'},
+      {'label': 'kiwi', 'value': 'kiwi'},
+    ];
+    List forlistview = [
+      {
+        'imagePath': 'cakephotos/cokkies.webp',
+        'name': 'cokkies',
+        'price': 233.45
+      },
+      {
+        'imagePath': 'cakephotos/cokkies2.jpg',
+        'name': 'cokkies',
+        'price': 10.44
+      },
+      {
+        'imagePath': 'cakephotos/cokkies3.jfiif',
+        'name': 'cokkies',
+        'price': 233.45
+      },
+      {
+        'imagePath': 'cakephotos/cokkies.webp',
+        'name': 'cokkies',
+        'price': 233.45
+      },
+    ];
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          // buttonBackgroundColor: Colors.,
+          color: Colors.black,
+          backgroundColor: Colors.white,
+          items: <Widget>[
+            InkWell(
+              onTap: () {},
+              child: Icon(Icons.cake_outlined, size: 30, color: Colors.white),
+            ),
+            Icon(Icons.add, size: 30, color: Colors.white),
+            Icon(Icons.list, size: 30, color: Colors.white),
+            Icon(Icons.compare_arrows, size: 30, color: Colors.white),
+          ],
+          onTap: (index) {
+            //Handle button tap
+          },
+        ),
+        body: Column(
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      alignment: Alignment.center,
+                      // color: Colors.yellow,
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Chose Cokkies\nFor Your Tea Party",
+                        style: GoogleFonts.getFont(
+                          'Nunito',
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(left: 10),
-                        // color: Colors.pink,
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: ElevatedButton(
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10),
+                      // color: Colors.pink,
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: ElevatedButton(
                             style: ButtonStyle(
                                 backgroundColor:
                                     MaterialStatePropertyAll(Colors.amber),
@@ -91,82 +122,79 @@ class _MyHomePageState extends State<MyHomePage> {
                               Icons.shopping_cart,
                               color: Colors.white,
                             ),
-                            onPressed: () {},
-                          ),
-                        ),
-                        // color: Colors.pink,
+                            onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginPage()))),
                       ),
+                      // color: Colors.pink,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  for (var i in l1) Center(child: CustomForExpand2(icon: i)),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: CustomForExpand3(
+                          icon: Icons.monitor_outlined,
+                          alignment: Alignment.centerLeft),
+                      flex: 2,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: CoolDropdown(
+                          dropdownList: dropdownItemList,
+                          onChange: (v) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginPage()));
+                          },
+                          defaultValue: dropdownItemList[3],
+                        ),
+                      ),
+                      flex: 3,
+                    ),
+                    Expanded(
+                      child: CustomForExpand3(
+                          icon: Icons.segment,
+                          alignment: Alignment.centerRight),
+                      flex: 2,
                     ),
                   ],
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    for (var i in l1) Center(child: CustomForExpand2(icon: i)),
-                  ],
-                ),
+            ),
+            Expanded(
+              flex: 6,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  for (var i in forlistview)
+                    CustomForExpand4(
+                        path: i['imagePath'],
+                        name: i['name'],
+                        price: i['price']),
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(5, 10, 5, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: CustomForExpand3(
-                            icon: Icons.monitor_outlined,
-                            alignment: Alignment.centerLeft),
-                        flex: 2,
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: DropdownButton(
-                            value: item[0],
-                            items: item.map(
-                              (item) {
-                                return DropdownMenuItem(
-                                  child: Text(
-                                    item,
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  value: item,
-                                );
-                              },
-                            ).toList(),
-                            onChanged: (country) {
-                              print("You selected: $country");
-                            },
-                          ),
-                        ),
-                        flex: 3,
-                      ),
-                      Expanded(
-                        child: CustomForExpand3(
-                            icon: Icons.segment,
-                            alignment: Alignment.centerRight),
-                        flex: 2,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 6,
-                child: ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    for (var i=0;i<=6;i++)
-                    CustomForExpand4(),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -197,70 +225,87 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget CustomForExpand4() {
-    return  Container(
-      height: 400,
-      width: 340,
-      // alignment: Alignment.center,
+  Widget CustomForExpand4(
+      {required path, required String name, required double price}) {
+    return Card(
+      color: Colors.pinkAccent,
       margin: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-        color: Colors.pinkAccent,
-      ),
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            'cakephotos/cake2.jpg',
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(20),
-                  height: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(40)),
-                    color: Colors.white.withOpacity(0.8),
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Hello",
-                      ),
-                      Spacer(),
-                      Container(
-                        margin: EdgeInsets.only(right: 5),
-                        // padding: EdgeInsets.all(10),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                              MaterialStatePropertyAll(Colors.black),
-                              padding: MaterialStatePropertyAll(
-                                  EdgeInsets.fromLTRB(35, 25, 35, 25)),
-                              shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50))),
+      child: SizedBox(
+        height: 400,
+        width: 340,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                path,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.fromLTRB(25, 20, 0, 0),
+              child: Text(
+                name,
+                style: TextStyle(fontSize: 50),
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(19),
+                    child: BlurryContainer(
+                      height: 70,
+                      blur: 10,
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                            child: Text(
+                              "\u{20B9} $price ",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            // padding: EdgeInsets.all(10),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(Colors.black),
+                                  padding: MaterialStatePropertyAll(
+                                      EdgeInsets.fromLTRB(35, 25, 35, 25)),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
                                     borderRadius: BorderRadius.horizontal(
                                       left: Radius.circular(40),
                                       right: Radius.circular(40),
                                     ),
                                   ))),
-                          child: Icon(
-                            Icons.add_shopping_cart,
-                          ),
-                          onPressed: () {},
-                        ),
-                      )
-                    ],
+                              child: Icon(
+                                Icons.add_shopping_cart,
+                              ),
+                              onPressed: () {},
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
+                  flex: 3,
                 ),
-                flex: 3,
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
